dfae3087d69c79bdf7898a7038515459ea74ad5f
Chris Johnson <cxjohnson@gmail.com>
2019-09-07 19:57:07 -0500

    Standardize to PDF Skeleton.

---
+++ bin/lintxml.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Checks an XML file for errors according to libxml extension.
+ *
+ * @package     cxj/lintxml
+ * @author      Chris Johnson <cxjohnson@gmail.com>
+ * @copyright   2017, Chris Johnson
+ * @license     MIT
+ */
+
+namespace Cxj\LintXml;
+
+
+/**
+ * Find the class autoloader.
+ */
+$autoFile = __DIR__ . '/../vendor/autoload.php';
+if (file_exists($autoFile)) {
+    require_once $autoFile;
+}
+else {
+    echo "DIR = " . __DIR__ . PHP_EOL;
+    die("Cannot find autoload.php.  Does 'composer install' need to be run?\n");
+}
+
+
+
+/// MAIN
+$main = new Main;
+try {
+    $status = $main->run();
+}
+catch (\Exception $e) {
+    echo 'Error: ' . $e->getMessage() . PHP_EOL;
+    $status = Status::SOFTWARE;
+}
+
+exit($status);
